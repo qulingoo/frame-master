ddfdb38b5beb9229b82ed98ea67ba0b608adab59
qulingkang <lingkang.qu@shuyun.com>
2017-09-18 14:27:26 +0800

    1

--- frame-redis-cache/src/main/java/com/qlk/redis/RedisModule.java
+++ frame-redis-cache/src/main/java/com/qlk/redis/RedisModule.java
@@ -1,5 +1,11 @@
 package com.qlk.redis;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 /**
  * redis模块 redis 连接池 redis 操作
  * 
@@ -8,11 +14,36 @@ package com.qlk.redis;
  */
 public class RedisModule {
 	private static RedisModule redisModule;
+	private JedisPool jedisPool;
 
 	private RedisModule() {
+		try {
+			jedisPool = jedisPool();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private JedisPool jedisPool() throws IOException {
+		Properties props = new Properties();
+
+		props.load(RedisModule.class.getClassLoader().getResourceAsStream("redis.properties"));
+
+		// 创建jedis池配置实例
+
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxIdle(Integer.valueOf(props.getProperty("jedis.pool.maxActive")));
+		config.setMaxIdle(Integer.valueOf(props.getProperty("jedis.pool.maxIdle")));
+		config.setMaxWaitMillis(Long.valueOf(props.getProperty("jedis.pool.maxWait")));
+		config.setTestOnBorrow(Boolean.valueOf(props.getProperty("jedis.pool.testOnBorrow")));
+		config.setTestOnReturn(Boolean.valueOf(props.getProperty("jedis.pool.testOnReturn")));
+		JedisPool pool = new JedisPool(config, props.getProperty("redis.ip"),
+				Integer.valueOf(props.getProperty("redis.port")), 20000, props.getProperty("redis.password"));
+		return pool;
 
 	}
-	
+
 	public static RedisModule getInstance() {
 
 		synchronized (RedisModule.class) {
